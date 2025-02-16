e46ebd4b922cbf08fad49374bc316e9ab5c3ee35
4beijing <maxdsply@gmail.com>
2025-02-15 20:16:44 -0800

    some button logic (wip)

--- ui.py
+++ ui.py
@@ -9,8 +9,8 @@ root.geometry("700x500")
 root.title("Interface")
 
 # Add a label with the text "no hands"
-label = tk.Label(root, text="no hands", font=("Helvetica_bold", 70))
-label.pack(padx=20, pady=20)
+main_screen = tk.Label(root, text="no hands", font=("Helvetica_bold", 70))
+main_screen.place(x=200, y=150)
 
 # Define the function to be triggered by the button
 def on_button_click():
@@ -24,7 +24,26 @@ def on_button_click():
 # Add a button to the window
 button = tk.Button(root, text="enable", font=("Helvetica", 40), command=on_button_click)
 button.pack(pady=20)
-button.place(x=250, y=190, width=200, height=100)
+button.place(x=200, y=190, width=200, height=100)
 
+settings_window = tk.Frame(root)
+
+def open_settings():
+    settings_window.pack(fill="both", expand=True)
+    settingsButton.pack_forget()
+    main_screen.pack_forget()
+
+
+def return_to_main():
+    main_screen.place(x=200, y=150)
+    settings_window.pack_forget()
+    returnButton.pack_forget()
+    settingsButton.place(x=400, y=190, height=100, width=100)
+
+settingsButton=tk.Button(root, text="⚙", font=("Helvetica", 40), command=open_settings)
+settingsButton.place(x=400, y=190, height=100, width=100)
+
+returnButton = tk.Button(settings_window, text="return", font=("Helvetica", 25), command=return_to_main)
+returnButton.pack(pady=20)
 # Start the Tkinter event loop
 root.mainloop()
