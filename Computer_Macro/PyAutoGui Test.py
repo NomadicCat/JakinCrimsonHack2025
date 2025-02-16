581c1def3e0833cab6fd1a596a13edeff988d03b
PineappleJammingg <cemendoza412@gmail.com>
2025-02-15 17:58:10 -0800

    Update PyAutoGui Test.py
    Added Window Call Script

--- Computer_Macro/PyAutoGui Test.py
+++ Computer_Macro/PyAutoGui Test.py
@@ -1,1 +1,20 @@
-import pyautogui
+import pyautogui, win32gui, win32con, time, pygetwindow, pyperclip
+
+def get_active_window():
+    # Get Active Window Title
+    active_window = gw.getActiveWindow()
+    if not active_window:
+        return None
+
+    # Simulate Ctrl + L (select address bar) and Ctrl + C (Copy URL)
+    pyautogui.hotkey('ctrl', 'l')
+    time.sleep(0.5) # Waits for UT to respond
+    pyautogui.hotkey('ctrl', 'c')
+    time.sleep(0.5)
+
+    # Returns URL from Clipboard
+    url = pyperclip.paste()
+    return url
+
+print(get_active_window()) # Window URL for future reference
+
